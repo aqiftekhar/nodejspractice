7e76928e513fb9cbc96e2218d029f46eb97ddc7a
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-17 20:45:54 +0500

    Add Routing to middleware

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-//const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-//add Request middlewhere
-app.use((req,res,next)=>{
-console.log("in the middlewhare");
-next(); // allow the request to continue to the next middleware in the line
+app.use('/',(req, res, next)=>{
+    console.log("This always runs!!");
+    next();
+})
+
+app.use('/products',(req,res,next)=>{
+    console.log("in the Products");
+    res.send('<h1>Hello From Producs</h1>');
 });
 
-app.use((req,res,next)=>{
-    console.log("in the another middlewhare");
-    res.send('<h1> Hello Express JS </h1>'); //Send response using middlewhare
+app.use('/',(req,res,next)=>{
+    console.log("in the Home");
+    res.send('<h1> Hello Express JS </h1>'); 
 });
-
-//const server = http.createServer(app); //We don't need to create sever because express listen function does this for us.
 
 app.listen(3000);
