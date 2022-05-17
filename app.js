f7300b25960c3e3ddacabfdcd132151011b01cfd
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-17 19:56:14 +0500

    Add  middlewares

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require('express');
 
 const app = express();
 
+//add Request middlewhere
+app.use((req,res,next)=>{
+console.log("in the middlewhare");
+next(); // invoke next method to allow request to travel on next middleware in the line
+});
+
+app.use((req,res,next)=>{
+    console.log("in the another middlewhare");
+    next();
+});
+
 const server = http.createServer(app);
 
 app.listen(3000);
