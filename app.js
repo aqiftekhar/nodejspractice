76ae39e2d77eb46d0fb4f5adc128ffb67189d950
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-17 21:28:28 +0500

    Limiting middleware execution to POST request

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use('/',(req, res, next)=>{
 app.use('/users',(req,res,next)=>{
     res.send('<form action="/addUser" method="POST"><input type="text" name="username"/><button type="submit">Add User</button></form>');
 });
-
-app.use('/addUser',(req, res, next)=>{
+//Instead of app.use, we should use http verbs i.e. app.post/app.get
+app.post('/addUser',(req, res, next)=>{
     console.log(req.body);
     res.redirect('/');
 });
