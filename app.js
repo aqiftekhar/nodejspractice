96e94dd3d6f3154eb19007ca3c5a115e1ec0f8ff
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-17 20:02:18 +0500

    Send response from middleware

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const app = express();
 //add Request middlewhere
 app.use((req,res,next)=>{
 console.log("in the middlewhare");
-next(); // invoke next method to allow request to travel on next middleware in the line
+next(); // allow the request to continue to the next middleware in the line
 });
 
 app.use((req,res,next)=>{
     console.log("in the another middlewhare");
-    next();
+    res.send('<h1> Hello Express JS </h1>'); //Send response using middlewhare
 });
 
 const server = http.createServer(app);
