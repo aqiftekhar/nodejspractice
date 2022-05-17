cbb27ba108a8bcf69284317b2a3639e53b3fbccd
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-18 02:21:54 +0500

    Implement root path on Shop route

--- routers/shop.js
+++ routers/shop.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 
+const root = require("../helper/path");
+
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname , '../', 'views', 'shop.html')); 
+    res.sendFile(path.join(root, 'views', 'shop.html')); 
 });
 module.exports = router;
