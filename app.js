f1d74fdc438715842c6795085df0f2de581c6875
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-18 01:02:37 +0500

    implement 404 Page not found

--- app.js
+++ app.js
@@ -15,4 +15,8 @@ app.use('/',(req, res, next)=>{
 app.use(adminRouts);
 app.use(shopRouters);
 
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page nor found</h1>');
+});
+
 app.listen(3000);
