f7ba731e21e3d16e621856d82f08950d4c11a6a2
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-22 00:22:21 +0500

    Refactored File Storage Code and Completed MVC implentation

--- models/Product.js
+++ models/Product.js
@@ -2,42 +2,39 @@ const fs = require('fs');
 const path = require('path');
 const root = require('../utils/path');
 
+const localpath = path.join(
+    root,
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = callback => {
+
+    fs.readFile(localpath, (error, contnet) => {
+        if (error) {
+            return callback([]);
+        }
+         callback(JSON.parse(contnet));
+    })
+};
+
 class Product {
     constructor(productName){
         this.productName = productName;
     }
 
     save = () => {
-        const localpath = path.join(
-            root,
-            'data',
-            'products.json'
-        );
-
-        fs.readFile(localpath, (error, content) => {
-            let products = [];
-            if (!error) {
-                products = JSON.parse(content);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(localpath, JSON.stringify(products), (error)=> {
                 console.log(error);
             });
         });
     }
+
     static getAll = (callback) => {
-        const localpath = path.join(
-            root,
-            'data',
-            'products.json'
-        );
-        fs.readFile(localpath, (error, contnet) => {
-            if (error) {
-                callback([]);
-            }
-             callback(JSON.parse(contnet));
-        })
-        // return products;
+        getProductsFromFile(callback);
     }
 }
+
 module.exports = Product;
