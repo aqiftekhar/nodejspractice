dc3d264c483810af82f6ea491b49aa135e995211
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-28 00:37:29 +0500

    Added MySQL Datbase Class

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+//const mysql = require('./utils/mysqldb');
+
 const adminRoutes = require('./routers/admin');
 const shopRouters = require('./routers/shop');
 
@@ -11,6 +13,8 @@ app.set('view engine', 'ejs');
 app.set('views','views');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//mysql.execute('SELECT * FROM MY_TABLE');
 
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
